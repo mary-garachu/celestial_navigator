667ccfb05ce01960baab410e47a3959bb948891a
mburuxx <njihiamburu.3@gmail.com>
2024-03-21 14:28:07 +0300

    Added hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Home from './Pages/Home'
+import Home from './Pages/Home';
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
+import Hero from './components/Hero'; 
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Hero/>
       <Home/>
     </div>
   );
---
+++ src/components/Hero/index.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import './index.css';
+
+const Hero = () => {
+  const [imageUrl, setImageUrl] = useState('');
+  const [imageDescription, setImageDescription] = useState('');
+  const [showDescription, setShowDescription] = useState(false);
+
+  useEffect(() => {
+    const apiUrl = 'https://api.nasa.gov/planetary/apod';
+    const apiKey = 'dEwYN7I7GTGRsWY8Zlc1Zrff5Uploy8oz2kMnPfi';
+
+    fetch(`${apiUrl}?api_key=${apiKey}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setImageUrl(data.url);
+        setImageDescription(data.explanation);
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  }, []);
+
+  const toggleDescription = () => {
+    setShowDescription(!showDescription);
+  };
+
+  return (
+    <section className="hero" style={{ backgroundImage: `url(${imageUrl})` }}>
+      <div className="day-picture">
+        {imageUrl && <img src={imageUrl} alt="NASA Picture of the Day" />}
+      </div>
+      <div className="astronomy-text" onClick={toggleDescription}>
+        <h1 className="big-text">Astronomy image<br/>of the day</h1>
+        <button className="learn-more-btn" onClick={toggleDescription}>
+          Learn More
+        </button>
+      </div>
+      {showDescription && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={toggleDescription}>&times;</span>
+            <p>{imageDescription}</p>
+          </div>
+        </div>
+      )}
+      <nav className="nav-bar">
+        <a href="#">Home</a>
+        <a href="#">Moon</a>
+        <a href="#">Stars</a>
+        <a href="#">Planets</a>
+      </nav>
+    </section>
+  );
+};
+
+export default Hero;
